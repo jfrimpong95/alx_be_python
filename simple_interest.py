721532eb7efb683a746ad7ab9051797c297b9f68
Frimpong Joseph <josephfrimpong91@gmail.com>
2025-11-09 21:29:10 +0000

    Fix syntax error in simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,7 +1,7 @@
-principal = 1000
+'principal = 1000
 rate = 0.05
 time = 3
 
 interest  = principal * rate * time
 
-print('the simple interest is:', interest)
+print('the simple interest is:', interest)'
