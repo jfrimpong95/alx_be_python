bb7d884c9b9e02683dbfe88e555e644942f7358f
Frimpong Joseph <josephfrimpong91@gmail.com>
2025-11-16 20:54:19 +0000

    Add files via upload

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,15 @@
+# basic_operations.py
+
+# Define the numbers
 number1 = 10
 number2 = 5
 
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
 
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
-Addition of 10 and 5 is 15
-Subtraction of 10 and 5 is 5
-Multiplication of 10 and 5 is 50
+# Print the results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
