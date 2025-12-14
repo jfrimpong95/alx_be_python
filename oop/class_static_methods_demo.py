a9ba5e347464ca0d061db27c1d06caf9656b7db5
Frimpong Joseph <josephfrimpong91@gmail.com>
2025-12-14 18:12:08 +0000

    Add files via upload

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,16 @@
+# class_static_methods_demo.py
+
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        """Returns the sum of two numbers."""
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        """Prints calculation type and returns the product of two numbers."""
+        print(cls.calculation_type)
+        return a * b
